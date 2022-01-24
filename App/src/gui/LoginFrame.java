141d628a6cd9c8b0c54c5b0c24575aa56e354a10
Rob <rob.19@libero.it>
2022-01-24 23:20:53 +0100

    WIP APP

--- App/src/gui/LoginFrame.java
+++ App/src/gui/LoginFrame.java
@@ -14,12 +14,18 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 import java.awt.Color;
 import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
 
 public class LoginFrame extends JFrame {
 
 	private Controller controller;
 	
-	private JPanel pannelloPrincipale;
+	private JPanel panelPrincipale;
+	private JPanel panelSecondario;
 	
 	public LoginFrame(Controller controller) {
 		this.controller = controller;
@@ -34,6 +40,17 @@ public class LoginFrame extends JFrame {
 	
 	private void inizializzaComponenti() {
 		
+		impostaFrame();
+		
+		impostaPanelPrincipale();
+		
+		impostaPanelSecondario();
+		
+		impostaLayoutPanelPrincipale();
+		
+	}
+	
+	private void impostaFrame() {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		}
@@ -45,15 +62,45 @@ public class LoginFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		setLocationRelativeTo(null);
+	}
+	
+	private void impostaPanelPrincipale() {
+		panelPrincipale = new JPanel();
+		panelPrincipale.setBackground(Color.LIGHT_GRAY);
+		panelPrincipale.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(panelPrincipale);
+	}
+	
+	private void impostaPanelSecondario() {
+		panelSecondario = new JPanel();
+		panelSecondario.setBackground(Color.WHITE);
 		
-		pannelloPrincipale = new JPanel();
-		pannelloPrincipale.setBackground(Color.WHITE);
-		pannelloPrincipale.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(pannelloPrincipale);
-		pannelloPrincipale.setLayout(null);
+		JButton btnProva = new JButton("New button");
+		btnProva.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(153, 163, 85, 21);
-		pannelloPrincipale.add(btnNewButton);
+		panelSecondario.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panelSecondario.add(btnProva);
+	}
+	
+	private void impostaLayoutPanelPrincipale() {
+		GroupLayout gl_panelPrincipale = new GroupLayout(panelPrincipale);
+		gl_panelPrincipale.setHorizontalGroup(
+			gl_panelPrincipale.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelPrincipale.createSequentialGroup()
+					.addGap(33)
+					.addComponent(panelSecondario, GroupLayout.DEFAULT_SIZE, 379, Short.MAX_VALUE)
+					.addGap(28))
+		);
+		gl_panelPrincipale.setVerticalGroup(
+			gl_panelPrincipale.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_panelPrincipale.createSequentialGroup()
+					.addGap(188)
+					.addComponent(panelSecondario, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(41))
+		);
+		panelPrincipale.setLayout(gl_panelPrincipale);
 	}
 }
