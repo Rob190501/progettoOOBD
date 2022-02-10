2e16de726f553845f1f71a665ed0ba889fe0c5f4
Rob190501 <rob.19@libero.it>
2022-02-10 18:38:57 +0100

    WIP

---
+++ APP/progettoOOBD/src/main/java/dto/Studente.java
@@ -0,0 +1,52 @@
+package dto;
+
+import java.util.LinkedList;
+
+public class Studente {
+    private String matricola;
+    private String nome;
+    private String cognome;
+    private LinkedList<Corso> corsiFrequentati;
+    private LinkedList<Lezione> presenze;
+
+    public Studente(String matricola, String nome, String cognome) {
+        setMatricola(matricola);
+        setNome(nome);
+        setCognome(cognome);
+    }
+    
+    public String getMatricola() {
+        return matricola;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public LinkedList<Corso> getCorsiFrequentati() {
+        return corsiFrequentati;
+    }
+
+    public LinkedList<Lezione> getPresenze() {
+        return presenze;
+    }
+
+    public void setMatricola(String matricola) {
+        this.matricola = matricola;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+    
+    
+    
+}
